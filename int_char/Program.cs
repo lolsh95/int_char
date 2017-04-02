4eb0fc605eb08fd0f9a39c8c68ab4be531de639d
LIFE-BOOK <LIFE-BOOK@LIFE-BOOK-PC>
2017-04-02 09:47:18 +0300

    Add project files.

---
+++ int_char/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace int_char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charMin = char.MinValue; //لمعرفة القيمة الادنى للكاركتر
+            int charMax = char.MaxValue;// لمعرفة القيمة القصوى للكركتر
+
+            char value = 'A';
+            int Vo = 65;
+            int vo1 = 71; //لتحويل الارقام الى حروف 
+            char myVal = Convert.ToChar(Vo); //نسخدم هذا لطريقة 
+
+            Console.Write("\nValue Min = {0}\nValue Max = {1}\nValues = {2}\nMY Valuee = {3}", charMin, charMax, value,myVal);
+            Console.ReadKey();
+        }
+    }
+}
